7feeeba1484a34db2b794c8e098c05182b5b4964
Abhay Ramkrushna Patil <patilabhay484@gmail.com>
2025-04-28 18:51:41 +0530

    Render a component

--- 01viteReact/src/App.jsx
+++ 01viteReact/src/App.jsx
@@ -1,6 +1,11 @@
+import Temp from "./Temp."
+
 function App() {
   return (
+   <>
     <h1>Abhay Ramkrushna Patil</h1>
+    <Temp/>
+   </>
   );
 }
 
---
+++ 01viteReact/src/Temp.jsx
@@ -0,0 +1,8 @@
+function Temp(){
+
+    return(
+        <h1>Abhay Patil</h1>
+    )
+}
+
+export default Temp ;
