5cc76de531992cd5d19c15ecd37f24348ebf74f3
Abhay Ramkrushna Patil <patilabhay484@gmail.com>
2025-04-28 23:26:32 +0530

    Created Custom React App

---
+++ customReact/customReact.js
@@ -0,0 +1,24 @@
+function customRender(reactElement, mainContainer) {
+    const domElement = document.createElement(reactElement.type);
+    domElement.innerHTML = reactElement.children; // Fix typo
+
+    // Set attributes properly
+    for (const prop in reactElement.props) {
+        domElement.setAttribute(prop, reactElement.props[prop]);
+    }
+
+    mainContainer.appendChild(domElement);
+}
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit Google'
+}
+
+const mainContainer = document.querySelector('#root');
+
+customRender(reactElement, mainContainer);
