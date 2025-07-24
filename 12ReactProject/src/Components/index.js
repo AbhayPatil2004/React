b6bd4e8bc5a167cea9b4254e5e74d6c123896d79
Abhay Ramkrushna Patil <patilabhay484@gmail.com>
2025-07-24 23:42:48 +0530

    custom React

--- 12ReactProject/src/Components/index.js
+++ 12ReactProject/src/Components/index.js
@@ -2,10 +2,12 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
 import Logo from './Logo'
+import Logout from "./Header/Logout";
 
 export {
     Header ,
     Footer ,
     Container ,
-    Logo
+    Logo,
+    Logout
 }
---
+++ 12ReactProject/src/index.js
@@ -0,0 +1,30 @@
+import Select from "./Components/Select";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Components/Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./Components/RTE";
+import Signup from "./Components/Signup";
+import Login from "./Components/Login";
+import Button from "./Button";
+import PostForm from "./Components/post-form/PostForm";
+import PostCard from "./Components/PostCard";
+import AuthLayout from "./Components/AuthLayout";
+import Input from "./Input";
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    Button,
+    PostForm,
+    PostCard,
+    AuthLayout, 
+    Input,
+    Select,
+}
